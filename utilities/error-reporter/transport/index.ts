6161ffd682f872cd274ecf7c88980b61dceb1f84
Jonas <jonasrauch23@gmail.com>
2025-01-27 16:44:54 +0100

    fix: export type to allow isolatedModules setting of ts

--- utilities/error-reporter/transport/index.ts
+++ utilities/error-reporter/transport/index.ts
@@ -2,25 +2,28 @@ import { LogEntry, LogLevel } from "../reporter";
 import { Formatter } from "../formatter";
 
 export type TransportConfig = {
-    level?: LogLevel
-    formatter?: Formatter
-}
+  level?: LogLevel;
+  formatter?: Formatter;
+};
 
 export interface Transport {
-    sendLog: (entrie: LogEntry) => Promise<void>
+  sendLog: (entrie: LogEntry) => Promise<void>;
 }
 
-import { TelegramTransport, TelegramTransportConfig } from "./telegramTransport";
+import {
+  TelegramTransport,
+  TelegramTransportConfig,
+} from "./telegramTransport";
 import { AxiosTransport, AxiosTransportConfig } from "./axiosTransport";
 import { ConsoleTransport, ConsoleTransportConfig } from "./consoleTransport";
 
-export {
-    TelegramTransport,
-    TelegramTransportConfig,
-    AxiosTransport,
-    AxiosTransportConfig,
-    ConsoleTransport,
-    ConsoleTransportConfig,
+export type {
+  TelegramTransport,
+  TelegramTransportConfig,
+  AxiosTransport,
+  AxiosTransportConfig,
+  ConsoleTransport,
+  ConsoleTransportConfig,
 };
 
 export type ClientTransports = AxiosTransport | ConsoleTransport;
