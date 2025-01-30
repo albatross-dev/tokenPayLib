a7f1818f2c9b720bb6892d43283bc2a3dfd53143
Jonas <jonasrauch23@gmail.com>
2025-01-30 19:44:18 +0100

    feat: telegram message pagination added

--- utilities/error-reporter/formatter/index.ts
+++ utilities/error-reporter/formatter/index.ts
@@ -5,41 +5,120 @@ export type FormattedLog = { log: any };
 export type Formatter = (entrie: LogEntry) => FormattedLog;
 
 export const defaultFormatter: Formatter = (entry: LogEntry) => {
-    return { log: entry };
-}
+  return { log: entry };
+};
 
 export const stringifyFormatter: Formatter = (entry: LogEntry) => {
-    return { log: JSON.stringify(entry) };
-}
+  return { log: JSON.stringify(entry) };
+};
 
 export const telegramHTMLFormatter: Formatter = (entry: LogEntry) => {
-
-    return { log: `
-<b>Log-Level: </b>${LogLevelStrings.get(entry.level) || entry.level.toString()}\n
+  const formatHeader = (page: number) => {
+    return `
+<b>Page: </b> ${page}\n
+<b>Log-Level: </b>${
+      LogLevelStrings.get(entry.level) || entry.level.toString()
+    }\n
 <b>Logger: </b>${entry.loggerName}\n
-<b>Message: </b>${entry.message}\n
 <b>Timestamp: </b>${entry.timestamp}\n
-<b>Data: </b>
-<pre>${prettifyJSONString(entry.data)}</pre>\n\n` + (
-        entry.userInformation 
-            ? `<b>Browser Information: </b>\n<pre>${prettifyJSONString(entry.userInformation)}</pre>` 
-            : ""
-        )
-    }
-}
+        `;
+  };
 
+  const formatMessage = (message: string) => {
+    return `<b>Message: </b>${message}\n`;
+  };
 
-function prettifyJSONString(s: any) {
-    try {
-        return JSON.stringify(JSON.parse(s), null, 2)
-            .replace(/&/g, "&amp;")
-            .replace(/</g, "&lt;")
-            .replace(/>/g, "&gt;")
-            .replace(/"/g, "&quot;")
-            .replace(/'/g, "&#39;")
-            .replace(/\n/g, "\\n")
-            .replace(/\\/g, "\\\\");
-    } catch (error) {
-        return s;
+  const formatData = (dataString: string) => {
+    return `<b>Data: </b>\n<pre>${dataString}</pre>\n\n`;
+  };
+
+  const formatBrowserInformation = (infoString: string) => {
+    return `<b>Browser Information: </b>\n<pre>${infoString}</pre>`;
+  };
+
+  let stringifiedData: string = entry.data
+    ? prettifyJSONString(entry.data)
+    : "";
+  let stringifiedInfo: string = entry.userInformation
+    ? prettifyJSONString(entry.userInformation)
+    : "";
+  let stringifiedMessage = entry.message;
+
+  const messages: string[] = [];
+  let currentMessage = "";
+  let currentField: "message" | "data" | "info" = "message";
+  let done = false;
+
+  for (let page = 1; !done; page++) {
+    currentMessage = formatHeader(page);
+
+    let messageHasSpace = true;
+    while (messageHasSpace) {
+      switch (currentField) {
+        case "message": {
+          if (stringifiedMessage.length === 0) {
+            currentField = "data";
+          } else {
+            const availableCharacters = 4096 - currentMessage.length - 17;
+            currentMessage += formatMessage(
+              stringifiedMessage.substring(0, availableCharacters)
+            );
+            stringifiedMessage =
+              stringifiedMessage.substring(availableCharacters);
+          }
+
+          break;
+        }
+        case "data": {
+          if (stringifiedData.length === 0) {
+            currentField = "info";
+          } else {
+            const availableCharacters = 4096 - currentMessage.length - 27;
+            currentMessage += formatData(
+              stringifiedData.substring(0, availableCharacters)
+            );
+            stringifiedData = stringifiedData.substring(availableCharacters);
+          }
+          break;
+        }
+        case "info": {
+          if (stringifiedInfo.length === 0) {
+            done = true;
+            break;
+          } else {
+            const availableCharacters = 4096 - currentMessage.length - 40;
+            currentMessage += formatBrowserInformation(
+              stringifiedInfo.substring(0, availableCharacters)
+            );
+            stringifiedInfo = stringifiedInfo.substring(availableCharacters);
+          }
+          break;
+        }
+      }
+
+      if (currentMessage.length > 4096 - 200 || done) {
+        messages.push(currentMessage);
+        messageHasSpace = false;
+      }
     }
+  }
+
+  return {
+    log: messages,
+  };
+};
+
+function prettifyJSONString(s: any): string {
+  try {
+    return JSON.stringify(JSON.parse(s), null, 2)
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#39;")
+      .replace(/\n/g, "\\n")
+      .replace(/\\/g, "\\\\");
+  } catch (error) {
+    return JSON.stringify(s);
+  }
 }
--- utilities/error-reporter/transport/telegramTransport.ts
+++ utilities/error-reporter/transport/telegramTransport.ts
@@ -1,84 +1,103 @@
-import axios from "axios"
-import { LogEntry, LogLevel } from "../reporter"
-import { defaultFormatter, Formatter } from "../formatter"
-import { Transport, TransportConfig } from "."
+import axios from "axios";
+import { LogEntry, LogLevel } from "../reporter";
+import { defaultFormatter, Formatter } from "../formatter";
+import { Transport, TransportConfig } from ".";
 
 export type TelegramParseMode = "MakrdownV2" | "HTML";
 
 export type TelegramTransportConfig = {
-    botToken: string
-    chatId: string
-    parseMode?: TelegramParseMode
+  botToken: string;
+  chatId: string;
+  parseMode?: TelegramParseMode;
 } & TransportConfig;
 
+const MAX_MESSAGE_LENGTH: number = 4096;
+
 export class TelegramTransport implements Transport {
-    private botToken: string;
-    private chatId: string;
-    private parseMode?: TelegramParseMode;
-    private formatter: Formatter = defaultFormatter;
-    private level: LogLevel = LogLevel.INFO;
+  private botToken: string;
+  private chatId: string;
+  private parseMode?: TelegramParseMode;
+  private formatter: Formatter = defaultFormatter;
+  private level: LogLevel = LogLevel.INFO;
 
-    constructor({ botToken, chatId, parseMode, formatter, level }: TelegramTransportConfig) {
-        this.botToken = botToken;
-        this.chatId = chatId;
+  constructor({
+    botToken,
+    chatId,
+    parseMode,
+    formatter,
+    level,
+  }: TelegramTransportConfig) {
+    this.botToken = botToken;
+    this.chatId = chatId;
 
-        if (formatter) {
-            this.formatter = formatter;
-        }
-
-        if (level) {
-            this.level = level;
-        }
-
-        if (parseMode) {
-            this.parseMode = parseMode;
-        }
+    if (formatter) {
+      this.formatter = formatter;
     }
 
-    async sendLog(entrie: LogEntry) {
-        // If the log level of this entrie is lower then the transports log level do nothing
-        if (entrie.level < this.level) {
-            return;
-        }
-
-        const formattedLog = this.formatter(entrie);
-
-        console.log(formattedLog);
-
-        try {
-            const response = await axios({
-                method: 'post',
-                url: `https://api.telegram.org/bot${this.botToken}/sendMessage`,
-                data: {
-                    chat_id: this.chatId,
-                    text: formattedLog.log,
-                    parse_mode: this.parseMode,
-                }
-            })
-
-            if (response.status !== 200) {
-                console.error('Failed to send message with Http Status: ', response.status, " ", response.statusText)
-                return
-            }
-
-            const data = response.data
-
-            if (data.ok !== true) {
-                console.error('Failed to send message with Telegram Error: ', data.description)
-                return
-            }
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                console.error('Failed to send message with Axios Error: ', error.response?.data || error.message)
-            } else {
-                console.error('Failed to send message with Unexpected Error: ', error)
-            }
-        }
+    if (level) {
+      this.level = level;
     }
+
+    if (parseMode) {
+      this.parseMode = parseMode;
+    }
+  }
+
+  async sendLog(entrie: LogEntry) {
+    // If the log level of this entrie is lower then the transports log level do nothing
+    if (entrie.level < this.level) {
+      return;
+    }
+
+    const formattedLog = this.formatter(entrie);
+
+    console.log(formattedLog);
+
+    try {
+      const response = await axios({
+        method: "post",
+        url: `https://api.telegram.org/bot${this.botToken}/sendMessage`,
+        data: {
+          chat_id: this.chatId,
+          text: formattedLog.log,
+          parse_mode: this.parseMode,
+        },
+      });
+
+      if (response.status !== 200) {
+        console.error(
+          "Failed to send message with Http Status: ",
+          response.status,
+          " ",
+          response.statusText
+        );
+        return;
+      }
+
+      const data = response.data;
+
+      if (data.ok !== true) {
+        console.error(
+          "Failed to send message with Telegram Error: ",
+          data.description
+        );
+        return;
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error(
+          "Failed to send message with Axios Error: ",
+          error.response?.data || error.message
+        );
+      } else {
+        console.error("Failed to send message with Unexpected Error: ", error);
+      }
+    }
+  }
 }
 
-
 export function isTelegramTransportConfig(config: any): boolean {
-    return typeof config.botToken === 'string'
-        && typeof config.chatId === 'string'
+  return (
+    typeof config.botToken === "string" && typeof config.chatId === "string"
+  );
 }
