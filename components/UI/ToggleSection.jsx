9a9bfeb6648fdebfac044f7ea2a075790b9001d8
Joshua Schmidt <joshuaschmidt@Joshuas-MacBook-Air.local>
2025-01-31 17:11:38 -0300

    togglesection

---
+++ components/UI/ToggleSection.jsx
@@ -0,0 +1,54 @@
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from '@headlessui/react';
+import { FiChevronDown } from 'react-icons/fi';
+import AnimateHeight from 'react-animate-height';
+import { useEffect, useState } from 'react';
+
+export default function ToggleSection({ title, content, storageKey }) {
+  const [isOpen, setIsOpen] = useState(() => {
+    if (typeof window !== 'undefined') {
+      const storedState = localStorage.getItem(storageKey);
+      return storedState ? JSON.parse(storedState) : true; // Default open
+    }
+    return true; // Default open when SSR
+  });
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem(storageKey, JSON.stringify(isOpen));
+    }
+  }, [isOpen, storageKey]);
+
+  return (
+    <div className="w-full max-w-4xl mx-auto">
+      <div className="bg-blue-200 shadow-md rounded-lg">
+        <Disclosure defaultOpen={isOpen}>
+          {({ open }) => (
+            <div className="">
+              <DisclosureButton
+                className="w-full flex items-center justify-between py-3 px-4 text-left font-medium"
+                onClick={() => setIsOpen((prev) => !prev)}
+              >
+                <div>{title}</div>
+                <FiChevronDown
+                  className={`w-6 h-6 text-blue-500 transition-transform ${isOpen ? 'rotate-180' : ''}`}
+                />
+              </DisclosureButton>
+
+              <AnimateHeight duration={300} height={isOpen ? 'auto' : 0}>
+                <DisclosurePanel static>
+                  <div className="p-8 bg-blue-100 text-gray-700">
+                    {content}
+                  </div>
+                </DisclosurePanel>
+              </AnimateHeight>
+            </div>
+          )}
+        </Disclosure>
+      </div>
+    </div>
+  );
+}
