f1ce0f729f561b59205edaeab635b37a09c00c15
Joshua Schmidt <joshuaschmidt@Joshuas-MacBook-Air.local>
2025-05-06 08:01:44 +0200

    push

--- components/transaction/partner/koywe/KoyweAwaitingFiatPayment.tsx
+++ components/transaction/partner/koywe/KoyweAwaitingFiatPayment.tsx
@@ -14,16 +14,13 @@ export default function KoyweAwaitingFiatPayment({
   refetch: () => void;
 }) {
   const { t: tTransaction } = useTranslation("transaction");
-  const [isLoading, setIsLoading] = useState<LoadingButtonStates>("normal");
-  const [error, setError] = useState<LoadingButtonError | null>(null);
+
   return (
     <div>
       <div>
         {transaction.burnAddress && (
           <div>
-            <div>
-              Bitte bezahle die angezeigte Summe an den angezeigten Adresse.
-            </div>
+            <div>{tTransaction("koyweAwaitingFiatPayment.description")}</div>
             <p className="text-xl font-bold bg-gray-100 rounded p-4 m-2">
               {transaction.burnAddress}
             </p>
@@ -33,15 +30,14 @@ export default function KoyweAwaitingFiatPayment({
         {transaction.providedAction && !transaction.burnAddress && (
           <div className="flex flex-col ">
             <div className="text-sm text-gray-500">
-              Klicken Sie auf den Button um die Zahlung zu bestätigen um auf die
-              Zahlungseite zu gelangen.
+              {tTransaction("koyweAwaitingFiatPayment.payNowDescription")}
             </div>
             <div className="pt-4">
               <Link
                 className="mt-4 btn btn-primary"
                 href={transaction.providedAction}
               >
-                Jetzt bezahlen
+                {tTransaction("koyweAwaitingFiatPayment.payNow")}
               </Link>
             </div>
           </div>
